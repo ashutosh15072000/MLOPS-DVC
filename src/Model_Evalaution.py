1e19d2ab1c094fbbbaad39c6e65c81fea3021399
ashutosh15072000 <ashutoshs019@gmail.com>
2025-07-25 14:26:17 +0530

    experiment tracking using dvclive

--- src/Model_Evalaution.py
+++ src/Model_Evalaution.py
@@ -5,6 +5,8 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score,precision_score,recall_score,roc_auc_score
 import logging
+import yaml
+from dvclive import Live
 
 ## ENSURE THE log dirctorary Exists
 log_dir='logs'
@@ -27,6 +29,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) ->dict:
+    """Load parameters from a YAML file"""
+    try:
+            with open(params_path, 'r') as file:
+                params = yaml.safe_load(file)
+                logger.debug("Parameters retrieved from %s",params_path)
+                return params
+    except FileNotFoundError:
+        logger.error("File not found: %s",params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error("Error parsing YAML file: %s",e)
+        raise
+    except Exception as e:
+        logger.debug("Unexpected error %s",e)
+        raise
 
 def load_model(file_path: str):
       """Load the trained model from a file."""
@@ -94,6 +113,7 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
+          params=load_params(params_path="params.yaml")
           clf=load_model('./models/random_forest_model.pkl')
           test_data= load_data('./data/processed/test_tfidf.csv')
 
@@ -101,8 +121,14 @@ def main():
           y_test=test_data.iloc[:,-1].values
           
           metrics=evaluate_model(clf,x_test,y_test)
-          
-          save_metrics(metrics,'reports/metrics.json')
+          ## EXPERIEMENT TRACKING USING DVCLIVE
+          with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+
+            live.log_params(params)
+            save_metrics(metrics,'reports/metrics.json')
     
     except Exception as e:
         logger.error('Failed to Compelete the Model Evaluation Process %s', e)
